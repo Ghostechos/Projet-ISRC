547b04b73c86463e5db3ac3b8f3f65667f0dd324
Piyas BARUA <piyas.barua@proton.me>
2026-06-04 00:27:04 +0200

    Fix main.tf slot + provider.tf IP + terraform.tfvars bridge vmbr1

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "proxmox" {
-  pm_api_url          = "https://192.168.1.226:8006/api2/json"
+  pm_api_url          = "https://192.168.1.115:8006/api2/json"
   pm_api_token_id     = "terraform@pve!terraform-token"
   pm_api_token_secret = var.proxmox_token_secret
   pm_tls_insecure     = true
