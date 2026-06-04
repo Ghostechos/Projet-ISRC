09e570cbf71751cb82ff8fa61b53c893de2f2440
Piyas BARUA <piyas.barua@proton.me>
2026-06-04 13:15:51 +0200

    Fix deploy.sh — DNS + chemins absolus

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# Fix DNS
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+
 echo ">>> Terraform apply..."
 cd /opt/Projet-ISRC/terraform
 terraform apply -parallelism=1 -auto-approve
@@ -12,7 +15,9 @@ git commit -m "Auto-deploy — $(date '+%Y-%m-%d %H:%M')" || echo "Rien à commi
 git push origin main
 
 echo ">>> Ansible deploy..."
-ansible-playbook -i ansible/inventory.ini ansible/playbook.yml \
+cd /opt/Projet-ISRC
+ansible-playbook -i /opt/Projet-ISRC/ansible/inventory.ini \
+  /opt/Projet-ISRC/ansible/playbook.yml \
   --private-key /root/.ssh/id_ed25519_isrc
 
 echo ">>> DONE !"
