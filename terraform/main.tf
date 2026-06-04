3ecda173e9b26573228e03c7ff30000cfc808e91
Piyas BARUA <piyas.barua@proton.me>
2026-06-04 13:24:29 +0200

    Auto-deploy — 2026-06-04 13:24

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,3 @@
-# ============================================================
-#  main.tf — 3 VMs projet ISRC LOGISTIA
-#  srv-web (103) et srv-db (104) déjà créés manuellement
-# ============================================================
-
 resource "proxmox_virtual_environment_vm" "srv_wazuh" {
   node_name = var.proxmox_node
   vm_id     = 105
@@ -11,10 +6,23 @@ resource "proxmox_virtual_environment_vm" "srv_wazuh" {
     vm_id = 100
     full  = true
   }
-  cpu { cores = 4; type = "x86-64-v2-AES" }
-  memory { dedicated = 4096 }
-  disk { datastore_id = var.storage; size = 100; interface = "scsi0" }
-  network_device { bridge = var.network_bridge; vlan_id = 30; model = "virtio" }
+  cpu {
+    cores = 4
+    type  = "x86-64-v2-AES"
+  }
+  memory {
+    dedicated = 4096
+  }
+  disk {
+    datastore_id = var.storage
+    size         = 100
+    interface    = "scsi0"
+  }
+  network_device {
+    bridge  = var.network_bridge
+    vlan_id = 30
+    model   = "virtio"
+  }
   initialization {
     user_account {
       username = "debian"
@@ -32,10 +40,23 @@ resource "proxmox_virtual_environment_vm" "srv_graylog" {
     vm_id = 100
     full  = true
   }
-  cpu { cores = 4; type = "x86-64-v2-AES" }
-  memory { dedicated = 4096 }
-  disk { datastore_id = var.storage; size = 100; interface = "scsi0" }
-  network_device { bridge = var.network_bridge; vlan_id = 30; model = "virtio" }
+  cpu {
+    cores = 4
+    type  = "x86-64-v2-AES"
+  }
+  memory {
+    dedicated = 4096
+  }
+  disk {
+    datastore_id = var.storage
+    size         = 100
+    interface    = "scsi0"
+  }
+  network_device {
+    bridge  = var.network_bridge
+    vlan_id = 30
+    model   = "virtio"
+  }
   initialization {
     user_account {
       username = "debian"
@@ -53,10 +74,23 @@ resource "proxmox_virtual_environment_vm" "srv_ia" {
     vm_id = 100
     full  = true
   }
-  cpu { cores = 4; type = "x86-64-v2-AES" }
-  memory { dedicated = 4096 }
-  disk { datastore_id = var.storage; size = 100; interface = "scsi0" }
-  network_device { bridge = var.network_bridge; vlan_id = 40; model = "virtio" }
+  cpu {
+    cores = 4
+    type  = "x86-64-v2-AES"
+  }
+  memory {
+    dedicated = 4096
+  }
+  disk {
+    datastore_id = var.storage
+    size         = 100
+    interface    = "scsi0"
+  }
+  network_device {
+    bridge  = var.network_bridge
+    vlan_id = 40
+    model   = "virtio"
+  }
   initialization {
     user_account {
       username = "debian"
